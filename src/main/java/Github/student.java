42f034757c6d4311d1f6fba207d240a469c9f0ff
Amani3D <najreed@me.com>
2019-10-14 12:55:57 -0400

    student

---
+++ src/main/java/Github/student.java
@@ -0,0 +1,38 @@
+package Github;
+
+import java.util.ArrayList;
+
+public class student {
+
+    private String firstName;
+    private String lastName;
+    private ArrayList<Double> examScores;
+
+    public student(String firstName, String lastName, ArrayList<Double> examScores) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.examScores = examScores;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public ArrayList<Double> getExamScores() {
+        return examScores;
+    }
+
+
+}
